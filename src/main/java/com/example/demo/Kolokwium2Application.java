2ff536742984b79eaf30c48fd285251d4d57ecd4
Miyukivv <natalia.kielarr@interia.pl>
2024-08-11 22:36:42 +0200

    initial commit

---
+++ src/main/java/com/example/demo/Kolokwium2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kolokwium2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kolokwium2Application.class, args);
+	}
+
+}
